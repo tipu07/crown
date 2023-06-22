90d462607b8e00402004e72aca55edd4b1321311
Tipu <30400782+tipu07@users.noreply.github.com>
2023-06-22 16:27:04 +0500

    Message

--- pages/dashboard/messages.tsx
+++ pages/dashboard/messages.tsx
@@ -2,20 +2,20 @@ import React from "react"
 import style from "@/styles/scss/web.module.scss"
 import DashboardHeader from "@/components/dashboardHeader"
 import DashboardSidebar from "@/components/dashboardSidebar"
-import AgreementDetail from "./agreement/agreementDetail"
+import MessagesDetail from "./messages/messagesDetail"
 
-const Agreement = () => {
+const Messages = () => {
 	return (
 		<>
-			<section className={style.dashboard} id={style.agreement}>
+			<section className={style.dashboard} id={style.message}>
 				<DashboardSidebar />
 				<div id={style.main_area}>
-					<DashboardHeader pageTitle="Agreement" />
-					<AgreementDetail />
+					<DashboardHeader pageTitle="Messages" />
+					<MessagesDetail />
 				</div>
 			</section>
 		</>
 	)
 }
 
-export default Agreement
+export default Messages
